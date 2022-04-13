4c0ac0093af3516e39dea4778e49933b5100a1c7
Sreevardhan Taluchuri <sreevardhan.t@virinnovations.in>
2022-04-13 15:35:34 +0530

    Started using middleware

--- express.js
+++ express.js
@@ -3,6 +3,15 @@ const http = require("http");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In first middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In second middleware");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000, () => {
